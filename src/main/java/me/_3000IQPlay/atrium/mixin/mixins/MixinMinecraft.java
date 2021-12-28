8a07b40da46c2627b3d178567613621895403d38
PreparedSystem_32 <80098736+master7720@users.noreply.github.com>
2021-12-28 13:04:52 -0800

    fixed comp issue

--- src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinMinecraft.java
+++ src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinMinecraft.java
@@ -90,7 +90,7 @@ public abstract class MixinMinecraft {
         return !MultiTask.getInstance().isOn() && playerSP.isHandActive();
     }
 
-    @Redirect(method={"rightClickMouse"}, at=@At(value="INVOKE", target="Lnet/minecraft/client/multiplayer/PlayerControllerMP;getIsHittingBlock()Z", ordinal=0), require=1)
+    @Redirect(method={"rightClickMouse"}, at=@At(value="INVOKE", target="Lnet/minecraft/client/multiplayer/PlayerControllerMP;getIsHittingBlock()Z", ordinal=0))
     private boolean isHittingBlockHook(PlayerControllerMP playerControllerMP) {
         return !MultiTask.getInstance().isOn() && playerControllerMP.getIsHittingBlock();
     }
