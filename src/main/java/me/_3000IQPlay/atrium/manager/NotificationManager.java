a9747b07680540e51503414b770af65aaf8adcc0
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2021-12-28 14:27:24 +0100

    First Project

---
+++ src/main/java/me/_3000IQPlay/atrium/manager/NotificationManager.java
@@ -0,0 +1,27 @@
+package me._3000IQPlay.atrium.manager;
+
+import me._3000IQPlay.atrium.Atrium;
+import me._3000IQPlay.atrium.features.modules.client.HUD;
+import me._3000IQPlay.atrium.features.notifications.Notifications;
+
+import java.util.ArrayList;
+
+public class NotificationManager {
+    private final ArrayList<Notifications> notifications = new ArrayList();
+
+    public void handleNotifications(int posY) {
+        for (int i = 0; i < this.getNotifications().size(); ++i) {
+            this.getNotifications().get(i).onDraw(posY);
+            posY -= Atrium.moduleManager.getModuleByClass(HUD.class).renderer.getFontHeight() + 5;
+        }
+    }
+
+    public void addNotification(String text, long duration) {
+        this.getNotifications().add(new Notifications(text, duration));
+    }
+
+    public ArrayList<Notifications> getNotifications() {
+        return this.notifications;
+    }
+}
+
---
+++ src/main/java/me/_3000IQPlay/atrium/manager/TotemPopManager.java
@@ -0,0 +1,211 @@
+package me._3000IQPlay.atrium.manager;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+import me._3000IQPlay.atrium.Atrium;
+import me._3000IQPlay.atrium.features.Feature;
+import me._3000IQPlay.atrium.features.command.Command;
+import me._3000IQPlay.atrium.features.modules.client.Notifications;
+import me._3000IQPlay.atrium.features.modules.client.ModuleTools;
+import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TotemPopManager
+        extends Feature {
+    private Notifications notifications;
+    private Map<EntityPlayer, Integer> poplist = new ConcurrentHashMap<EntityPlayer, Integer>();
+    private final Set<EntityPlayer> toAnnounce = new HashSet<EntityPlayer>();
+
+    public void onUpdate() {
+        if (this.notifications.totemAnnounce.passedMs(this.notifications.delay.getValue().intValue()) && this.notifications.isOn() && this.notifications.totemPops.getValue().booleanValue()) {
+            for (EntityPlayer player : this.toAnnounce) {
+                if (player == null) continue;
+                int playerNumber = 0;
+                for (char character : player.getName().toCharArray()) {
+                    playerNumber += character;
+                    playerNumber *= 10;
+                }
+                Command.sendOverwriteMessage(this.pop(player), playerNumber, this.notifications.totemNoti.getValue());
+                this.toAnnounce.remove(player);
+                this.notifications.totemAnnounce.reset();
+                break;
+            }
+        }
+    }
+
+    public String pop(EntityPlayer player) {
+        if (this.getTotemPops(player) == 1) {
+            if (ModuleTools.getInstance().isEnabled()) {
+                switch (ModuleTools.getInstance().popNotifier.getValue()) {
+                    case FUTURE: {
+                        String text = ChatFormatting.RED + "[Future] " + ChatFormatting.GREEN + player.getName() + ChatFormatting.GRAY + " just popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.GRAY + " totem.";
+                        return text;
+                    }
+                    case ATRIUM: {
+                        String text = ChatFormatting.AQUA + player.getName() + ChatFormatting.WHITE + " popped " + ChatFormatting.AQUA + this.getTotemPops(player) + ChatFormatting.WHITE + " totem.";
+                        return text;
+                    }
+                    case DOTGOD: {
+                        String text = ChatFormatting.DARK_PURPLE + "[" + ChatFormatting.LIGHT_PURPLE + "DotGod.CC" + ChatFormatting.DARK_PURPLE + "] " + ChatFormatting.LIGHT_PURPLE + player.getName() + " has popped " + ChatFormatting.RED + this.getTotemPops(player) + ChatFormatting.LIGHT_PURPLE + " time in total!";
+                        return text;
+                    }
+                    case NONE: {
+                        return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totem.";
+                    }
+                }
+            } else {
+                return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totem.";
+            }
+        } else {
+            if (ModuleTools.getInstance().isEnabled()) {
+                switch (ModuleTools.getInstance().popNotifier.getValue()) {
+                    case FUTURE: {
+                        String text = ChatFormatting.RED + "[Future] " + ChatFormatting.GREEN + player.getName() + ChatFormatting.GRAY + " just popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.GRAY + " totems.";
+                        return text;
+                    }
+                    case ATRIUM: {
+                        String text = ChatFormatting.AQUA + player.getName() + ChatFormatting.WHITE + " popped " + ChatFormatting.AQUA + this.getTotemPops(player) + ChatFormatting.WHITE + " totems.";
+                        return text;
+                    }
+                    case DOTGOD: {
+                        String text = ChatFormatting.DARK_PURPLE + "[" + ChatFormatting.LIGHT_PURPLE + "DotGod.CC" + ChatFormatting.DARK_PURPLE + "] " + ChatFormatting.LIGHT_PURPLE + player.getName() + " has popped " + ChatFormatting.RED + this.getTotemPops(player) + ChatFormatting.LIGHT_PURPLE + " times in total!";
+                        return text;
+                    }
+                    case NONE: {
+                        return ChatFormatting.WHITE + player.getName() + " popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totems.";
+                    }
+                }
+            } else {
+                return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " popped " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totems.";
+            }
+        }
+        return "";
+    }
+
+
+    public void onLogout() {
+        this.onOwnLogout(this.notifications.clearOnLogout.getValue());
+    }
+
+    public void init() {
+        this.notifications = Atrium.moduleManager.getModuleByClass(Notifications.class);
+    }
+
+    public void onTotemPop(EntityPlayer player) {
+        this.popTotem(player);
+        if (!player.equals(TotemPopManager.mc.player)) {
+            this.toAnnounce.add(player);
+            this.notifications.totemAnnounce.reset();
+        }
+    }
+
+    public String death1(EntityPlayer player) {
+        if (this.getTotemPops(player) == 1) {
+            if (ModuleTools.getInstance().isEnabled()) {
+                switch (ModuleTools.getInstance().popNotifier.getValue()) {
+                    case FUTURE: {
+                        String text = ChatFormatting.RED + "[Future] " + ChatFormatting.GREEN + player.getName() + ChatFormatting.GRAY + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.GRAY + " totem.";
+                        return text;
+                    }
+                    case ATRIUM: {
+                        String text = ChatFormatting.AQUA + player.getName() + ChatFormatting.WHITE + " died after popping " + ChatFormatting.AQUA + this.getTotemPops(player) + ChatFormatting.WHITE + " totem.";
+                        return text;
+                    }
+                    case DOTGOD: {
+                        String text = ChatFormatting.DARK_PURPLE + "[" + ChatFormatting.LIGHT_PURPLE + "DotGod.CC" + ChatFormatting.DARK_PURPLE + "] " + ChatFormatting.LIGHT_PURPLE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.LIGHT_PURPLE + " time!";
+                        return text;
+                    }
+                    case NONE: {
+                        return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totem!";
+
+                    }
+                }
+            } else {
+                return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totem!";
+
+            }
+        } else {
+            if (ModuleTools.getInstance().isEnabled()) {
+                switch (ModuleTools.getInstance().popNotifier.getValue()) {
+                    case FUTURE: {
+                        String text = ChatFormatting.RED + "[Future] " + ChatFormatting.GREEN + player.getName() + ChatFormatting.GRAY + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.GRAY + " totems.";
+                        return text;
+                    }
+                    case ATRIUM: {
+                        String text = ChatFormatting.AQUA + player.getName() + ChatFormatting.WHITE + " died after popping " + ChatFormatting.AQUA + this.getTotemPops(player) + ChatFormatting.WHITE + " totems.";
+                        return text;
+                    }
+                    case DOTGOD: {
+                        String text = ChatFormatting.DARK_PURPLE + "[" + ChatFormatting.LIGHT_PURPLE + "DotGod.CC" + ChatFormatting.DARK_PURPLE + "] " + ChatFormatting.LIGHT_PURPLE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.LIGHT_PURPLE + " times!";
+                        return text;
+                    }
+                    case NONE: {
+                        return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totems!";
+
+                    }
+                }
+            } else {
+                return Atrium.commandManager.getClientMessage() + ChatFormatting.WHITE + player.getName() + " died after popping " + ChatFormatting.GREEN + this.getTotemPops(player) + ChatFormatting.WHITE + " Totems!";
+            }
+        }
+        return null;
+    }
+
+
+    public void onDeath(EntityPlayer player) {
+        if (this.getTotemPops(player) != 0 && !player.equals(TotemPopManager.mc.player) && this.notifications.isOn() && this.notifications.totemPops.getValue().booleanValue()) {
+            int playerNumber = 0;
+            for (char character : player.getName().toCharArray()) {
+                playerNumber += character;
+                playerNumber *= 10;
+            }
+            Command.sendOverwriteMessage(this.death1(player), playerNumber, this.notifications.totemNoti.getValue());
+            this.toAnnounce.remove(player);
+        }
+        this.resetPops(player);
+    }
+
+    public void onLogout(EntityPlayer player, boolean clearOnLogout) {
+        if (clearOnLogout) {
+            this.resetPops(player);
+        }
+    }
+
+    public void onOwnLogout(boolean clearOnLogout) {
+        if (clearOnLogout) {
+            this.clearList();
+        }
+    }
+
+    public void clearList() {
+        this.poplist = new ConcurrentHashMap<EntityPlayer, Integer>();
+    }
+
+    public void resetPops(EntityPlayer player) {
+        this.setTotemPops(player, 0);
+    }
+
+    public void popTotem(EntityPlayer player) {
+        this.poplist.merge(player, 1, Integer::sum);
+    }
+
+    public void setTotemPops(EntityPlayer player, int amount) {
+        this.poplist.put(player, amount);
+    }
+
+    public int getTotemPops(EntityPlayer player) {
+        Integer pops = this.poplist.get(player);
+        if (pops == null) {
+            return 0;
+        }
+        return pops;
+    }
+
+    public String getTotemPopString(EntityPlayer player) {
+        return "\u00a7f" + (this.getTotemPops(player) <= 0 ? "" : "-" + this.getTotemPops(player) + " ");
+    }
+}
+
