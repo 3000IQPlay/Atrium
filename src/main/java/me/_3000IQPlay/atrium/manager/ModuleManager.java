793c125e9be87b1d100c1f743dd8796a9801c008
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-01-19 16:04:09 +0100

    First Project

--- src/main/java/me/_3000IQPlay/atrium/manager/ModuleManager.java
+++ src/main/java/me/_3000IQPlay/atrium/manager/ModuleManager.java
@@ -131,6 +131,8 @@ public class ModuleManager
 		this.modules.add(new AutoCity());
 	    	this.modules.add(new AntiLog4j());
 		this.modules.add(new AutoKickBow());
+	        this.modules.add(new ChorusPredict());
+		this.moduleColorMap.put(this.getModuleByClass(ChorusPredict.class), new Color(43, 165, 216));
 		this.moduleColorMap.put(this.getModuleByClass(AutoKickBow.class), new Color(164, 63, 254));
 		this.moduleColorMap.put(this.getModuleByClass(AntiLog4j.class), new Color(74, 83, 46));
 		this.moduleColorMap.put(this.getModuleByClass(AutoCity.class), new Color(97, 173, 162));
