b10f2b98fc4e65006dd5827edca6b92949e70f0e
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-01-30 00:00:23 +0100

    ServerModule = PingBypass

--- src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinC00Handshake.java
+++ src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinC00Handshake.java
@@ -1,6 +1,6 @@
 package me._3000IQPlay.atrium.mixin.mixins;
 
-import me._3000IQPlay.atrium.features.modules.client.ServerModule;
+import me._3000IQPlay.atrium.features.modules.client.PingBypass;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.handshake.client.C00Handshake;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,11 +11,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class MixinC00Handshake {
     @Redirect(method={"writePacketData"}, at=@At(value="INVOKE", target="Lnet/minecraft/network/PacketBuffer;writeString(Ljava/lang/String;)Lnet/minecraft/network/PacketBuffer;"))
     public PacketBuffer writePacketDataHook(PacketBuffer packetBuffer, String string) {
-        if (ServerModule.getInstance().noFML.getValue().booleanValue()) {
+        if (PingBypass.getInstance().noFML.getValue().booleanValue()) {
             String ipNoFML = string.substring(0, string.length() - "\u0000FML\u0000".length());
             return packetBuffer.writeString(ipNoFML);
         }
         return packetBuffer.writeString(string);
     }
 }
-
