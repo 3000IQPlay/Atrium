27869e83406bf4614099244b0317b559a1a5f041
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-01-29 23:59:21 +0100

    ServerModule = PingBypass

--- src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinServerAddress.java
+++ src/main/java/me/_3000IQPlay/atrium/mixin/mixins/MixinServerAddress.java
@@ -1,6 +1,6 @@
 package me._3000IQPlay.atrium.mixin.mixins;
 
-import me._3000IQPlay.atrium.features.modules.client.ServerModule;
+import me._3000IQPlay.atrium.features.modules.client.PingBypass;
 import me._3000IQPlay.atrium.mixin.mixins.accessors.IServerAddress;
 import net.minecraft.client.multiplayer.ServerAddress;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class MixinServerAddress {
     @Redirect(method={"fromString"}, at=@At(value="INVOKE", target="Lnet/minecraft/client/multiplayer/ServerAddress;getServerAddress(Ljava/lang/String;)[Ljava/lang/String;"))
     private static String[] getServerAddressHook(String ip) {
-        ServerModule module;
+        PingBypass module;
         int port;
-        if (ip.equals(ServerModule.getInstance().ip.getValue()) && (port = (module = ServerModule.getInstance()).getPort()) != -1) {
-            return new String[]{ServerModule.getInstance().ip.getValue(), Integer.toString(port)};
+        if (ip.equals(PingBypass.getInstance().ip.getValue()) && (port = (module = PingBypass.getInstance()).getPort()) != -1) {
+            return new String[]{PingBypass.getInstance().ip.getValue(), Integer.toString(port)};
         }
         return IServerAddress.getServerAddress(ip);
     }
