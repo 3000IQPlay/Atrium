2945920b1141157055774ebb07a90e40d915f2b5
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2022-01-30 00:25:37 +0100

    ServerModule = PingBypass

--- src/main/java/me/_3000IQPlay/atrium/features/modules/combat/Offhand.java
+++ src/main/java/me/_3000IQPlay/atrium/features/modules/combat/Offhand.java
@@ -255,7 +255,7 @@ public class Offhand
     @SubscribeEvent
     public void onPacketReceive(PacketEvent.Receive event) {
         SPacketSetSlot packet;
-        if (ServerModule.getInstance().isConnected() && event.getPacket() instanceof SPacketSetSlot && (packet = event.getPacket()).getSlot() == -1 && packet.getWindowId() != -1) {
+        if (PingBypass.getInstance().isConnected() && event.getPacket() instanceof SPacketSetSlot && (packet = event.getPacket()).getSlot() == -1 && packet.getWindowId() != -1) {
             ((IContainer) Offhand.mc.player.openContainer).setTransactionID((short) packet.getWindowId());
             ((ISPacketSetSlot) packet).setWindowId(-1);
             this.serverTimer.reset();
